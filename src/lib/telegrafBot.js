fc6a6bf4f779ad34c844f4a906f9cf09faf4e5f0
Lenin Garizabalo <lcamargo@protonmail.com>
2023-04-23 15:59:31 -0500

    Update telegrafBot.js

--- src/lib/telegrafBot.js
+++ src/lib/telegrafBot.js
@@ -1,6 +1,6 @@
 const { Telegraf, Markup } = require('telegraf')
 
-const bot = new Telegraf('6207808284:AAGz3HRnpeN0OD4tSv6CM8lIHP8tiCNrpPY')
+const bot = new Telegraf('')
 
 module.exports = {
   Bot: bot,
