3fd2fa3c63ce330195b027467f566fe6acd363de
Lenin Garizabalo <lcamargo@protonmail.com>
2023-04-23 15:40:29 -0500

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,5 @@
 module.exports = {
-  channelId: '-1001911571641',
-  channelLogsId: '-1001871389242',
-  dbUrl:
-    'mongodb+srv://jobbank_:Bk4FUlgMuK9lpNsY@cluster0.yhaaogz.mongodb.net/jobbank'
-  // dbUrl:
-  //   'mongodb+srv://jobbank:iTJ9uoCzOagbxoQm@cluster0.yhaaogz.mongodb.net/jobbank_test' // dev
+  channelId: '',
+  channelLogsId: '',
+  dbUrl: ''
 }
